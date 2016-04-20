a66dc55f509ee2547ae3819cb069a1d15452f27a
Pablo Bollansée <pablo.bollansee@gmail.com>
2016-04-20 14:39:44 +0200

    Tester now actually does the user test. Gives output about how many correct and how many missed guesses there are.

--- Part 2/Tester.py
+++ Part 2/Tester.py
@@ -37,27 +37,40 @@ def do_per_user_test():
         #[item for sublist in l for item in sublist]
         test_set['learn'] = [other_files for other_user, other_files in filepaths_per_user.items() if other_user != user]
         test_set['learn'] = [x for y in test_set['learn'] for x in y]
+        test_set['user'] = user
         test_sets.append(test_set)
     
     #print("Test sets: {}".format(test_sets))
     
+    Guesser.max_number_of_guesses = 5
+    Guesser.use_derived_urls = True
+    
+    total_correct_guesses = 0
+    total_missed_guesses = 0
     for test_set in test_sets:
-        print("User Testing for: {}".format(test_set))
+        print("User Testing for: {}".format(test_set['user']))
         
         guesser = Guesser()
         guesser.learn_from_files(test_set['learn'])
         # do some guessing
-        total_correct_guesses = 0        
+        correct_guesses = 0
+        missed_guesses = 0
         for log_file in test_set['test']:
-            tester_log_file = TesterLogFile(log_file)
-            for info in tester_log_file.parsed_lines:
-                if info.type == "load":
-                    if tester_log_file.contains_guesses_from_for(guesser, info.url):
-                        total_correct_guesses += 1
-        print("Total correct guesses: {}".format(total_correct_guesses))
+            tester_log_file = TesterLogFile(log_file, use_derivatives = False)
+            for idx, url in enumerate(tester_log_file.load_urls[:-1]):
+                info_pairs = guesser.get_guesses(url)
+                guessed_urls = [url for [url, weight] in info_pairs]
+                if tester_log_file.contains_urls_for_guesses(guessed_urls, url, idx):
+                    correct_guesses += 1
+                else:
+                    missed_guesses += 1
+        total_correct_guesses += correct_guesses
+        total_missed_guesses += missed_guesses
+        print("{} correct guesses, {} missed guesses.".format(correct_guesses, missed_guesses))
         print()
         print()
-        
+    print("{} total correct guesses, {} total missed guesses".format(total_correct_guesses, total_missed_guesses))
+    
     # test
     ## guess some stuff
     ## calculate accuracy
@@ -65,19 +78,39 @@ def do_per_user_test():
 def find_all_csv_names():
     #all_csvs = glob.glob('./data/Our own/*.csv')
     #all_csvs = glob.glob('./data/test/*.csv')
-    all_csvs = glob.glob('./data/*.csv')
+    all_csvs = glob.glob('./data/*.csv') #user-testing assumes datafiles with "/.../uXX_XX.csv" format
     return all_csvs
 
 class TesterLogFile:
     
-    def __init__(self, filepath):
-        self.parsed_lines = []
+    def __init__(self, filepath, use_derivatives = True):
+        parsed_lines = []
         with open(filepath, 'r') as lines:
-            self.parsed_lines = [Util.parse_log_line(line) for line in lines]
-        self.parsed_lines = [info for info in self.parsed_lines if info is not None]
+            parsed_lines = [Util.parse_log_line(line) for line in lines]
+        parsed_lines = [info for info in parsed_lines if info is not None]
+        # get load urls as these are the ones we'll be testing on
+        self.load_urls = [info.url for info in parsed_lines if info.type == "load"]
+        self.use_derivatives = use_derivatives
+        #print(Load urls from {}: {}".format(filepath, self.load_urls))
     
-    def contains_guesses_from_for(self, guesser, for_url):
-        return True
+    def contains_urls_for_guesses(self, guesses, guessing_for_url, guessing_index = -1):
+        '''
+        guesses: a list of guesses
+        guessing_for_url: the url we're guessing for
+        guessing_index: the index of the url, if it's one from this log file, otherwise -1
+        '''
+        other_urls = self.load_urls.copy()
+        if guessing_index > 0:
+            other_urls = other_urls[guessing_index+1:]
+        
+        if self.use_derivatives:
+            # add derivatives
+           other_urls = [[url]+Util.get_derived_urls(url) for url in other_urls]
+           other_urls = [x for y in other_urls for x in y] #flatten
+        # find intersection
+        intersection = [i for i in guesses if i in other_urls]
+        #print("Guessed urls: {}, with intersection: {}".format(urls, intersection))
+        return len(intersection) > 0
 
 if __name__ == "__main__":
     sys.exit(main())
